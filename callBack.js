07bbf4839085dfd3f7f67e8e3ffc78ce076ff140
HrushikeshB-1121 <hrushikeshb2545@gmail.com>
2024-04-17 12:09:36 +0530

    [Hrushikesh] Added: Callback

--- callBack.js
+++ callBack.js
@@ -12,17 +12,38 @@
 //   myCalculator(5, 4, displaySum);
 
 
-function callback(){
-    console.log("callback function called")
-}
+// function callback(){
+//     console.log("callback function called")
+// }
 
-var add = function(a,b,callback){
-    console.log(a+b)
-    callback()
-}
+// var add = function(a,b,callback){
+//     console.log(a+b)
+//     callback()
+// }
 
-add(3,4,callback)
+// add(3,4,callback)
 
 // add(2,5,function callback(){
 //     console.log("hiiiiii")
+// })
+
+// //callback Hell
+
+// function getData(id,getnextId){
+//     console.log(`Getting data of ${id} ......`);
+//     setTimeout(()=> {
+//         console.log(`Data ${id}`);
+//         if(getnextId){
+//             getnextId();
+//         }
+//     },2000);
+// }
+
+
+// getData(1,()=>{
+//     getData(2,()=>{
+//         getData(3,()=>{
+//             getData(4);
+//         })
+//     })
 // })
