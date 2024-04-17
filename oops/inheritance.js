792f916def92679d897b8f9e6337f2e64555795e
HrushikeshB-1121 <hrushikeshb2545@gmail.com>
2024-04-17 12:08:41 +0530

    [Hrushikesh] Added: Inheritance

--- oops/inheritance.js
+++ oops/inheritance.js
@@ -1,97 +1,95 @@
 // Inheritance inherit properties and methods from other objects,
 
-// Prototype Chaining:
-
-//Constructor Function Inheritance:
-
-// Parent constructor function
-function Animal(name) {
-    this.name = name;
+// inheritance using the extends keyword 
+  
+// Superclass
+class Animal {
+    constructor(name) {
+      this.name = name;
+    }
+  
+    speak() {
+      console.log(this.name + ' makes a sound.');
+    }
   }
   
-  // Add method to parent prototype
-  Animal.prototype.speak = function() {
-    console.log(this.name + ' makes a sound.');
-  };
-
-  // Child constructor function
-  function Dog(name, breed) {
-    Animal.call(this, name); // Call parent constructor
-    this.breed = breed;
+  // Subclass
+  class Dog extends Animal {
+    constructor(name, breed) {
+      super(name); // Call superclass constructor
+      this.breed = breed;
+    }
+  
+    bark() {
+      console.log(this.name + ' barks.');
+    }
   }
   
   // Usage
   const animal = new Animal('Animal');
-  const dog = new Dog('Buddy', 'Golden Retriever');
+  const dog = new Dog('Rio', 'Golden Retriever');
   
   // Usage
   animal.speak(); 
+  dog.speak();    
+  dog.bark();
+
+
+//Constructor Function Inheritance:
 
 // // Parent constructor function
 // function Animal(name) {
 //     this.name = name;
 //   }
   
-// // Add method to parent prototype
+//   // Add method to parent prototype
 //   Animal.prototype.speak = function() {
 //     console.log(this.name + ' makes a sound.');
 //   };
-  
-// // Child constructor function
+
+//   // Child constructor function
 //   function Dog(name, breed) {
 //     Animal.call(this, name); // Call parent constructor
 //     this.breed = breed;
 //   }
   
-// // Set up prototype chain
-//   Dog.prototype = Object.create(Animal.prototype);
-//   Dog.prototype.constructor = Dog;
-  
-// // Add method to child prototype
-//   Dog.prototype.bark = function() {
-//     console.log(this.name + ' barks.');
-//   };
-  
-// // Create instances
-//   const animal = new Animal('Animal');
-//   const dog = new Dog('Rio', 'Golden Retriever');
-  
-// // Usage
-// animal.speak();
-// dog.speak();   
-// dog.bark();    
-  
-
-// inheritance using the extends keyword 
-  
-// Superclass
-// class Animal {
-//     constructor(name) {
-//       this.name = name;
-//     }
-  
-//     speak() {
-//       console.log(this.name + ' makes a sound.');
-//     }
-//   }
-  
-//   // Subclass
-//   class Dog extends Animal {
-//     constructor(name, breed) {
-//       super(name); // Call superclass constructor
-//       this.breed = breed;
-//     }
-  
-//     bark() {
-//       console.log(this.name + ' barks.');
-//     }
-//   }
-  
 //   // Usage
 //   const animal = new Animal('Animal');
-//   const dog = new Dog('Rio', 'Golden Retriever');
+//   const dog = new Dog('Buddy', 'Golden Retriever');
   
 //   // Usage
 //   animal.speak(); 
-//   dog.speak();    
-//   dog.bark();
+
+// // // Parent constructor function
+// // function Animal(name) {
+// //     this.name = name;
+// //   }
+  
+// // // Add method to parent prototype
+// //   Animal.prototype.speak = function() {
+// //     console.log(this.name + ' makes a sound.');
+// //   };
+  
+// // // Child constructor function
+// //   function Dog(name, breed) {
+// //     Animal.call(this, name); // Call parent constructor
+// //     this.breed = breed;
+// //   }
+  
+// // // Set up prototype chain
+// //   Dog.prototype = Object.create(Animal.prototype);
+// //   Dog.prototype.constructor = Dog;
+  
+// // // Add method to child prototype
+// //   Dog.prototype.bark = function() {
+// //     console.log(this.name + ' barks.');
+// //   };
+  
+// // // Create instances
+// //   const animal = new Animal('Animal');
+// //   const dog = new Dog('Rio', 'Golden Retriever');
+  
+// // // Usage
+// // animal.speak();
+// // dog.speak();   
+// // dog.bark();    
